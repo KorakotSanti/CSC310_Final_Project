454141146a48c53456bd8f66f0a14e23ea668cff
Korakot Santiudommongkol <korakotsanti@gmail.com>
2020-05-04 15:04:25 -0400

    complete

--- main.py
+++ main.py
@@ -1,37 +1,76 @@
 import tkinter as tk
+import numpy as np
 from PIL import Image, ImageDraw, ImageGrab
 from skimage.io import imread
 from skimage.exposure import rescale_intensity
 from skimage.transform import resize
+from joblib import load
 
 class DigitRecognition:
     
     def __init__(self):
+        # The entire GUI itself
         self.root = tk.Tk()
         self.root.title("Image Recognition")
-        self.canvas = tk.Canvas(self.root,width=500, height=350,highlightthickness=1, highlightbackground="blue", bg="black")
+
+        # add a Canvas where you will draw the digit
+        self.canvas = tk.Canvas(self.root,width=400, height=400,highlightthickness=3, highlightbackground="blue", bg="black")
+
+        # add a motion command so that when you hold down mouse button 1 it will draw
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.grid(row=0)
+
+        # The string value of result
         self.result = tk.StringVar()
         self.result.set("Empty")
+
+        # label to display the prediction
         self.label = tk.Label(self.root, textvariable=self.result).grid(row=1)
+
+        # predict Button
         self.predictB = tk.Button(self.root, text="Predict", width=25, bg="yellow", command=self.predictDigit).grid(row=2)
+
+        # clear Button, clear the canvas for new drawing
         self.clearB = tk.Button(self.root, text="Clear", width=25, bg="white", command=self.delete).grid(row=3)
-        self.image=Image.new("LA",(500,350))
+
+        # A new image that will be drawn
+        self.image = Image.new("LA",(400,400))
         self.draw=ImageDraw.Draw(self.image)
+
+        # load our classifer model
+        self.model = load('KNN_model.joblib')
+
+        # To run the GUI
         self.root.mainloop()
+
+    def imgprocess(self):
+        # need to resize image to 8x8
+        tempimg = np.array(self.image)
+        newimg = resize(tempimg, (8,8))
+
+        # rescale image from value of 0-255 to value 0-16
+        newimg = rescale_intensity(newimg, out_range=(0,16))
+
+        imgdata = np.array(newimg)
+        imgdata = imgdata.transpose(2,0,1)
+        imgdata = imgdata[0].reshape(1,-1)
+
+        return imgdata
 
     def predictDigit(self):
         # resize image and save as a png
         file='test.png'
-        img = self.image.resize((500,350))
-        img.save(file)
-        self.result.set("yes")
+        self.image.save(file)
+        testdata = self.imgprocess()
+        res = self.model.predict(testdata)
+
+        self.result.set(str(res[0]))
 
     def delete(self):
-        self.image=Image.new("LA",(500,350))
+        self.image=Image.new("LA",(400,400))
         self.draw=ImageDraw.Draw(self.image)
         self.canvas.delete('all')
+        self.result.set("empty")
 
     def paint(self, event):
         inkwidth=10
--- test.py
+++ test.py
@@ -1,33 +1,55 @@
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.utils import shuffle
+np.set_printoptions(formatter={'float_kind':"{:3.2f}".format})
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from assets.confint import classification_confint
+from sklearn.tree import tree
+from assets.treeviz import tree_print
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from joblib import dump
 from sklearn.datasets import load_digits
-from skimage.io import imread
-from skimage.exposure import rescale_intensity
-from skimage.transform import resize
-from PIL import Image
 
 digits = load_digits()
-print(digits.data[:1])
-model = KMeans(n_clusters=10, random_state=0)
-model.fit(digits.data)
+images = digits.data
+labels = digits.target
 
-print('---------------------------')
-print('---------------------------')
-print('---------------------------')
+# decision trees
+model2 = DecisionTreeClassifier(random_state=1)
 
-img = resize(imread("test.png"), (8,8))
-img = rescale_intensity(img, out_range=(0,16))
-x_test = np.array(img)
-print(x_test)
+# grid search
+param_grid = {'max_depth': list(range(1,21)), 'criterion': ['entropy','gini'] }
+grid = GridSearchCV(model2, param_grid, cv=5)
+grid.fit(images, labels)
+print("Grid Search: best parameters: {}".format(grid.best_params_))
 
-print('---------------------------')
-print('---------------------------')
-print('---------------------------')
-x_test = x_test.transpose(2,0,1)
-x_test = x_test[0].reshape(1,-1)
+# accuracy of best model with confidence interval
+best_model = grid.best_estimator_
+predict_y = best_model.predict(images)
+acc = accuracy_score(labels, predict_y)
+lb,ub = classification_confint(acc,images.shape[0])
+print("Accuracy: {:3.2f} ({:3.2f},{:3.2f})".format(acc,lb,ub))
 
-print(x_test)
-prediction = model.predict(x_test)
-print(prediction)
+dump(best_model, 'DTreeModel.joblib')
+
+# KNN
+model = KNeighborsClassifier()
+
+# grid search
+param_grid = {'n_neighbors': list(range(1,51))}
+grid = GridSearchCV(model, param_grid, cv=5)
+grid.fit(images, labels)
+print("Grid Search: best parameters: {}".format(grid.best_params_))
+
+# accuracy of best model with confidence interval
+best_model = grid.best_estimator_
+predict_y = best_model.predict(images)
+acc = accuracy_score(labels, predict_y)
+lb,ub = classification_confint(acc,images.shape[0])
+print("Accuracy: {:3.2f} ({:3.2f},{:3.2f})".format(acc,lb,ub))
+
+dump(best_model, 'KNN_Model.joblib')
